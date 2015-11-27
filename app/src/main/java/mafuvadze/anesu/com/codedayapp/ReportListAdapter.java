792c85bed1b17fb8a3621e00faf04ed3c93c3de4
Anesu <mafuvadzeanesu@gmail.com>
2015-11-27 13:17:13 -0600

    added report listview adapter

---
+++ app/src/main/java/mafuvadze/anesu/com/codedayapp/ReportListAdapter.java
@@ -0,0 +1,73 @@
+package mafuvadze.anesu.com.codedayapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Created by Angellar Manguvo on 11/27/2015.
+ */
+public class ReportListAdapter extends ArrayAdapter<String>
+{
+    private final String SPELLING_ERROR = "spelling";
+    private final String GRAMMER_ERROR = "grammer";
+    private final String PUNCTUATION_ERROR = "punctuation";
+    private final String DETAILS = "detail";
+    List<String> identifier;
+    List<Object> error;
+    Context context;
+    public ReportListAdapter(Context context, int resource, HashMap<String, Object> error) {
+        super(context, resource);
+        this.context = context;
+        identifier = new ArrayList<String>();
+        this.error = new ArrayList<Object>();
+        for(String id : error.keySet())
+        {
+            identifier.add(id);
+            this.error.add(error.get(id));
+        }
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        switch (identifier.get(position))
+        {
+            case SPELLING_ERROR:
+            {
+                if(convertView == null)
+                {
+                    convertView = LayoutInflater.from(context)
+                            .inflate(R.layout.spelling_error_row, parent, false);
+                }
+
+                String err = (String) error.get(position);
+                String txt_err = err + " may be misspelled";
+                TextView err_view = (TextView) convertView.findViewById(R.id.mispelled);
+                err_view.setText(txt_err);
+
+            }
+            case GRAMMER_ERROR:
+            {
+
+            }
+            case PUNCTUATION_ERROR:
+            {
+
+            }
+            case DETAILS:
+            {
+
+            }
+        }
+
+        return super.getView(position, convertView, parent);
+    }
+}
